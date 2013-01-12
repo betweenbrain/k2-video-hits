b3e5bbe28939b3300accd9b34fdb854ffd89df33
Matt Thomas <matt@betweenbrain.com>
2013-01-11 22:05:51 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-k2-video-hits
-=============
+K2 Video Hits Plugin
+=================
+
+A Joomla! 1.5/2.5 proof of concept pseudo-cron plugin to synchronize video view data from external providers with K2 item hits. 
+
+The plugin runs as a pseudo-cron job, fired every X number of minutes, or after, when your site has traffic.
+
+When executed, the plugin will loop through your K2 items and attempt to detect the video provider by inspecting the Media source field. When a provider is matched, the plugin then extracts the video ID from the embed code, contacts the provider and retrieves views recorded by the provider, and then updates the corresponding K2 item hits to reflect popularity as reported by the provider.
+
+Current vieo providers include:
+* YouTube
+* Brightcove (MAPI token required)
+
+Pseduo-cron[https://github.com/betweenbrain/pseudocron-plugin] functionality based on the FeedGator pseudo-cron plugin by Matt Faulds of Trafalgar Design (UK) Ltd.
---
+++ k2_video_hits.php
@@ -0,0 +1,233 @@
+<?php defined('_JEXEC') or die;
+/**
+ * File       k2_video_hits.php
+ * Created    1/11/13 1:52 PM
+ * Author     Matt Thomas
+ * Website    http://betweenbrain.com
+ * Email      matt@betweenbrain.com
+ * Support    https://github.com/betweenbrain/
+ * Copyright  Copyright (C) 2013 betweenbrain llc. All Rights Reserved.
+ * License    GNU GPL v3 or later
+ */
+
+jimport('joomla.plugin.plugin');
+jimport('joomla.html.parameter');
+
+class plgSystemk2_video_hits extends JPlugin {
+
+	/**
+	 * Hardcoded minimum of 5 minutes
+	 *
+	 * @var
+	 */
+	protected $interval = 300;
+
+	/**
+	 * An associative array to contain data about each video
+	 *
+	 * @param $videoData
+	 * @return array
+	 */
+	protected $videoData = NULL;
+
+	function plgSystemk2_video_hits(&$subject, $params) {
+		parent::__construct($subject, $params);
+
+		$this->plugin =& JPluginHelper::getPlugin('system', 'k2_video_hits');
+		$this->params = new JParameter($this->plugin->params);
+
+		// Reference the global database object
+		$this->db = JFactory::getDbo();
+
+		// Convert input into minutes
+		$this->interval = (int) ($this->params->get('interval', 5) * 60);
+
+		// Correct value if value is under the minimum
+		if ($this->interval < 300) {
+			$this->interval = 300;
+		}
+	}
+
+	/**
+	 * Function to set last run for pseudo-cron execution
+	 *
+	 * @internal param $now
+	 * @return bool
+	 */
+	private function runPseudoCron() {
+		$now  = JFactory::getDate();
+		$now  = $now->toUnix();
+		$last = $this->params->get('last_run');
+		$diff = $now - $last;
+
+		if ($diff > $this->interval) {
+
+			$version = new JVersion();
+			define('J_VERSION', $version->getShortVersion());
+			jimport('joomla.registry.format');
+			// Set pseudo-cron last execution time as a parameter, value of now
+			$this->params->set('last_run', $now);
+
+			if (J_VERSION >= 1.6) {
+				$handler = JRegistryFormat::getInstance('json');
+				$params  = new JObject();
+				$params->set('interval', $this->params->get('interval', 5));
+				$params->set('last_run', $now);
+				$params = $handler->objectToString($params, array());
+				// Update plugin parameters in databaseSpelling
+				$query = 'UPDATE #__extensions' .
+					' SET params=' . $this->db->Quote($params) .
+					' WHERE element = ' . $this->db->Quote('k2_video_hits') .
+					' AND folder = ' . $this->db->Quote('system') .
+					' AND enabled >= 1' .
+					' AND type =' . $this->db->Quote('plugin') .
+					' AND state >= 0';
+				$this->db->setQuery($query);
+				$this->db->query();
+			} else {
+				// Retrieve saved parameters from database
+				$query = ' SELECT params' .
+					' FROM #__plugins' .
+					' WHERE element = ' . $this->db->Quote('k2_video_hits') . '';
+				$this->db->setQuery($query);
+				$params = $this->db->loadResult();
+				// Check if last_run parameter has been recorded before.
+				if (preg_match('/last_run=/', $params)) {
+					// If it has been, update it.
+					$params = preg_replace('/last_run=([0-9]*)/', 'last_run=' . $now, $params);
+				} else {
+					// Add last_run parameter to database if it has not been recorded before.
+					// TODO: Currently adding last_run to beginning of param string due to extra "\n" when using $params .=
+					$params = 'last_run=' . $now . "\n" . $params;
+				}
+				// Update plugin parameters in database
+				$query = 'UPDATE #__plugins' .
+					' SET params=' . $this->db->Quote($params) .
+					' WHERE element = ' . $this->db->Quote('k2_video_hits') .
+					' AND folder = ' . $this->db->Quote('system') .
+					' AND published >= 1';
+				$this->db->setQuery($query);
+				$this->db->query();
+			}
+
+			return TRUE;
+		}
+
+		return FALSE;
+	}
+
+	/**
+	 * Function to retrieve YouTube video data
+	 *
+	 * @param $item
+	 * @return mixed
+	 */
+	private function getYoutubeVideoData($item) {
+		// Match part of the embed code to extract the YouTube Video ID
+		preg_match('/\/embed\/([a-zA-Z0-9-?]*)"/', $item['video'], $videoId);
+		// Retrieve data about this video from YouTube
+		$json = file_get_contents('https://gdata.youtube.com/feeds/api/videos/' . $videoId[1] . '?v=2&alt=json');
+		// Decode the JSON results
+		$results = json_decode($json, TRUE);
+		// Build the videoData array from the data from YouTube
+		$videoData['views'] = $results['entry']['yt$statistics']['viewCount'];
+
+		return $videoData;
+	}
+
+	/**
+	 * Function to retrieve Brightcove video data
+	 *
+	 * @param $item
+	 * @return mixed
+	 */
+	private function getBrightcoveVideoData($item) {
+		// Define Brightcove API token to access video data from our account
+		$brightcovetoken = htmlspecialchars($this->params->get('brightcovetoken'));
+		// Match part of the embed code to extract the Brightcove Video ID
+		preg_match('/@videoPlayer" value="([0-9]*)"/', $item['video'], $videoId);
+		// Retrieve data about this video from Brightcove
+		$json = file_get_contents('http://api.brightcove.com/services/library?command=find_video_by_id&video_id=' . $videoId[1] . '&video_fields=name,shortDescription,longDescription,publishedDate,lastModifiedDate,videoStillURL,length,playsTotal&token=' . $brightcovetoken);
+		// Decode the JSON results
+		$results = json_decode($json, TRUE);
+		// Build the videoData array from the data from Brightcove
+
+		$videoData['views'] = $results['playsTotal'];
+
+		return $videoData;
+	}
+
+	/**
+	 * Function to return video data based on enabled parameters and detected video provider type
+	 *
+	 * @param $item
+	 * @return mixed
+	 */
+	private function getVideoData($item) {
+		// If Brightcove is enabled and in the Media source field embed code
+		if (($this->params->get('brightcove')) && (strstr($item['video'], 'brightcove'))) {
+			$videoData = $this->getBrightcoveVideoData($item);
+		} // If YouTube is enabled and in the Media source field embed code
+		elseif (($this->params->get('youtube')) && (strstr($item['video'], 'youtube'))) {
+			$videoData = $this->getYoutubeVideoData($item);
+		} // In case the above conditions are not met, set videoData as NULL so we can bail out of updating the database
+		else {
+			$videoData = NULL;
+		}
+
+		return $videoData;
+	}
+
+	private function updateK2($videoData, $item) {
+		if ($videoData) {
+			// Update K2 with the video data from video provider.
+			$query = 'UPDATE #__k2_items' .
+				' SET hits = ' . $videoData['views'] .
+				' WHERE id = ' . $item['id'];
+
+			$this->db->setQuery($query);
+			$this->db->query();
+		}
+	}
+
+	function onAfterRoute() {
+		// Disable error reporting due to K2 issue, see http://code.google.com/p/getk2/issues/detail?id=431
+		// Notice: Undefined property: JObject::$url in /components/com_k2/models/item.php on line 498
+		ini_set("display_errors", 0);
+
+		$app = JFactory::getApplication();
+		// User defined set of K2 categories and exclusions to process
+		$k2categories = htmlspecialchars($this->params->get('k2category'));
+		$exclusions   = htmlspecialchars($this->params->get('exclusions'));
+		// Convert comma separated list into array while removing extra spaces
+		$k2categories = explode(',', preg_replace('/\s/', '', $k2categories));
+		$exclusions   = explode(',', preg_replace('/\s/', '', $exclusions));
+
+		// Display error message in back-end if K2 category parameter isn't defined
+		if ($app->isAdmin() && !$k2categories) {
+			// Add a message to the admin message queue
+			$app->enqueueMessage(JText::_('A K2 category has not been set for the k2_video_hits plugin.'), 'error');
+		}
+
+		if ($app->isSite() && $k2categories && $this->runPseudoCron()) {
+			// JSON of all K2 items
+			$json = file_get_contents(JURI::base() . '/index.php?option=com_k2&view=itemlist&layout=category&format=json');
+			// Decode the results as an associative array
+			$results = json_decode($json, TRUE);
+			// Get the items array
+			$items = $results['items'];
+
+			foreach ($items as $item) {
+				// Check that the item belongs to a category that we want to process and isn't excluded
+				if ((in_array($item['category']['id'], $k2categories)) && (!in_array($item['id'], $exclusions))) {
+					// Retrieve video data from the provider
+					$videoData = $this->getVideoData($item);
+					// Update K2 with the data retrieved from the provider
+					$this->updateK2($videoData, $item);
+				}
+			}
+		}
+
+		return FALSE;
+	}
+}
